24a72340a6080e0d980c26adc6612bf3f36db55a
Siber0817 <adam.nezveda@seznam.cz>
2024-04-08 00:39:10 +0200

    Přidejte soubory projektu.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using test.ViewModel;
+
+namespace test
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage(MainViewModel vm)
+        {
+            InitializeComponent();
+            BindingContext = vm;
+            
+        }
+
+        
+    }
+
+}
